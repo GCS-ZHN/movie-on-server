38a29859f67a634d627def4c8ba57ec7d302e65e
GCS-ZHN <zhang.h.n@foxmail.com>
2023-01-08 16:11:41 +0800

    定义APPtest

---
+++ src/test/java/top/gcszhn/movie/AppTest.java
@@ -0,0 +1,9 @@
+package top.gcszhn.movie;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public abstract class AppTest {}
--- src/test/java/top/gcszhn/movie/service/BaiduPanServiceTest.java
+++ src/test/java/top/gcszhn/movie/service/BaiduPanServiceTest.java
@@ -13,21 +13,16 @@ import org.apache.http.HttpStatus;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
+import top.gcszhn.movie.AppTest;
 import top.gcszhn.movie.utils.HttpDataPair;
 import top.gcszhn.movie.utils.LogUtils;
 
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class BaiduPanServiceTest {
+public class BaiduPanServiceTest extends AppTest {
     @Autowired
     BaiduPanService baiduPanService;
 
