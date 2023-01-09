3776d5c164c6922b2273270d3834195e523188b5
GCS-ZHN <zhang.h.n@foxmail.com>
2023-01-09 22:37:16 +0800

    support delay

--- src/main/java/top/gcszhn/movie/utils/WebBrowerUtils.java
+++ src/main/java/top/gcszhn/movie/utils/WebBrowerUtils.java
@@ -4,12 +4,23 @@ import java.net.URI;
 
 public class WebBrowerUtils {
     public static void openUrl(String url) {
+        openUrl(url, 0);
+    }
+    public static void openUrl(String url, long delay) {
         if (Desktop.isDesktopSupported()) {
-            try {
-                Desktop.getDesktop().browse(new URI(url));
-            } catch (Exception e) {
-                LogUtils.printMessage(null, e, LogUtils.Level.ERROR);
-            }
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(delay);
+                        Desktop.getDesktop().browse(new URI(url));
+                    } catch (Exception e) {
+                        LogUtils.printMessage(null, e, LogUtils.Level.ERROR);
+                    }
+                }
+            }.start();
+        } else {
+            LogUtils.printMessage("Current platform not support open web browser", LogUtils.Level.ERROR);
         }
     }
 }
